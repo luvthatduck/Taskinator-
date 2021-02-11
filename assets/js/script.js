f1384308720d9e3e71085ee440d26e22cda91026
Janell Myrick <76677736+luvthatduck@users.noreply.github.com>
2021-02-11 11:42:34 -0700

    load from local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,20 +39,14 @@ var taskFormHandler = function (event) {
 
 
 var createTaskEl = function (taskDataObj) {
-  //create list item 
+
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
-
-  // add task id as a custom attribute 
   listItemEl.setAttribute("data-task-id", taskIdCounter);
 
-  // create div to hold task info and add to list item
   var taskInfoEl = document.createElement("div");
-  // give it a class name 
   taskInfoEl.className = "task-info";
-  // add HTML content to div 
   taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
-
   listItemEl.appendChild(taskInfoEl);
 
 
@@ -224,7 +218,31 @@ var saveTasks = function () {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
+var loadTasks = function () {
+  // retrieve tasks from localstorage
+  var savedTasks = localStorage.getItem("tasks");
+  console.log();
+  // if there are no tasks, set tasks to an empty array and return out of the function
+  if (!savedTasks) {
+    return false;
+  }
+  console.log("Saved tasks found!");
+  // else, load up saved tasks
+
+  // parse into array of objects
+  savedTasks = JSON.parse(savedTasks);
+
+  // loop through savedTasks array
+  for (var i = 0; i < savedTasks.length; i++) {
+    // pass each task object into the `createTaskEl()` function
+    createTaskEl(savedTasks[i]);
+  }
+
+};
+
 
 pageContentEl.addEventListener("click", taskButtonHandler);
 
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
+
+loadTasks();
